98da8e04b132ec356cbe1d5101912c12dd580769
kevinrlutz <kevinlutz429@gmail.com>
2022-08-10 19:34:13 -0400

    Added admin login/auth

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const port = process.env.PORT
 const formidable = require('express-formidable')
 const mongoose = require('mongoose')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
 const publicDirPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -19,16 +23,65 @@ app.set('views', viewPath)
 hbs.registerPartials(partialsPath)
 
 app.use(formidable())
+app.use(cookieParser())
 
 app.use(express.static(publicDirPath))
 
+const jwtVerify = (cookies) => {
+	if (cookies.JWT === undefined) {
+		return false
+	}
+	return true
+}
+
 app.get('/', (req, res) => {
 	res.render('index', {
+		error: false,
+		pageTitle: 'Appointment Manager | Log In',
+		query: req.query,
+	})
+})
+
+app.post('/login', async (req, res) => {
+	const hashedAdminPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 8)
+
+	const isMatch = await bcrypt.compare(
+		req.fields.password,
+		hashedAdminPassword
+	)
+
+	if (req.fields.username !== process.env.ADMIN_USERNAME || !isMatch) {
+		return res.render('index', {
+			error: true,
+			pageTitle: 'Appointment Manager | Login',
+			query: req.query,
+		})
+	}
+
+	const token = jwt.sign(
+		{ _id: process.env.ADMIN_USERNAME },
+		process.env.JWT_SECRET
+	)
+
+	res.cookie('JWT', token, { maxAge: 600000 })
+	res.redirect('/home')
+})
+
+app.get('/home', (req, res) => {
+	if (!jwtVerify(req.cookies)) {
+		return res.redirect('/')
+	}
+
+	res.render('home', {
 		pageTitle: 'Appointment Manager',
 	})
 })
 
 app.get('/appointments', (req, res) => {
+	if (!jwtVerify(req.cookies)) {
+		return res.redirect('/')
+	}
+
 	request(
 		process.env.APPT_SERVICE_URL,
 		{ json: true },
@@ -42,6 +95,10 @@ app.get('/appointments', (req, res) => {
 })
 
 app.get('/appointments/create', (req, res) => {
+	if (!jwtVerify(req.cookies)) {
+		return res.redirect('/')
+	}
+
 	res.render('create-appt', {
 		pageTitle: 'Create an Appointment',
 	})
@@ -59,6 +116,10 @@ app.post('/appointments/create', (req, res) => {
 })
 
 app.get('/appointments/:apptId', (req, res) => {
+	if (!jwtVerify(req.cookies)) {
+		return res.redirect('/')
+	}
+
 	request(
 		process.env.APPT_SERVICE_URL + '/' + req.params.apptId,
 		{ json: true },
@@ -72,6 +133,10 @@ app.get('/appointments/:apptId', (req, res) => {
 })
 
 app.get('/appointments/:apptId/edit', (req, res) => {
+	if (!jwtVerify(req.cookies)) {
+		return res.redirect('/')
+	}
+
 	request(
 		process.env.APPT_SERVICE_URL + '/' + req.params.apptId,
 		{ json: true },
